dfac7ec35269b385cafe3db21a184387e9a7dc46
Moh Dev <73759551+MohamedCS1@users.noreply.github.com>
2022-11-28 23:55:04 +0100

    create events screen

---
+++ app/src/main/java/com/example/gdgchallenge/EventScheduleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gdgchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EventScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_schedule)
+    }
+}
--- app/src/main/java/com/example/gdgchallenge/MainActivity.kt
+++ app/src/main/java/com/example/gdgchallenge/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.gdgchallenge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gdgchallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.linearLayoutMain.setOnClickListener {
+            startActivity(Intent(this ,EventsActivity::class.java))
+        }
     }
 }
